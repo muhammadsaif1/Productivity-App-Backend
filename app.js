ce28bf96a5f504e6b7651436b6b79886ad223933
Aadil-Shabir <aadil.shabir13@gmail.com>
2024-05-11 01:41:16 +0500

    some fixes and comments

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+
+// remove lines from 7 to 9 when they are not being used in the file.
+// this is unnecessary code.
 const path = require("path");
 const mongoose = require("mongoose");
 const Task = require("./models/Task");
+
+const tasksRouter = require("./routes/createTaskRoute");
 const errorHandler = require("./middleware/errorHandler");
 const connectToDatabase = require("./config/database");
 const corsMiddleware = require("./middleware/corsHandler");
@@ -16,9 +21,14 @@ app.use(corsMiddleware);
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/createTask", require("./routes/createTaskRoute"));
+// made changes here. With your approach, we had a problem when adding more routes.
+// with this approach, you can make create as many routes as you want in the routes file.
+// remove line 27 when you read this.
+// app.use("/api/createTask", require("./routes/createTaskRoute"));
+app.use(tasksRouter);
+
 app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
-  console.log("THe server is running on port:3000");
+    console.log("THe server is running on port:3000");
 });
--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -3,29 +3,26 @@ const Task = require("../models/Task");
 // @ POST /api/createTask
 // @ access public
 
+// good job!
 const createTask = async (req, res) => {
-  try {
-    console.log(req.body);
-    const { title, description, deadline } = req.body;
-    if (!title || !description || !deadline) {
-      res
-        .status(400)
-        .json({ success: false, message: "All fields are Mandatory" });
-    }
-    const newTask = new Task({
-      title,
-      description,
-      deadline,
-    });
-    await newTask.save();
+    try {
+        console.log(req.body);
+        const { title, description, deadline } = req.body;
+        if (!title || !description || !deadline) {
+            res.status(400).json({ success: false, message: "All fields are Mandatory" });
+        }
+        const newTask = new Task({
+            title,
+            description,
+            deadline,
+        });
+        await newTask.save();
 
-    res
-      .status(201)
-      .json({ success: true, message: "Task created successfully" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ success: false, message: "Failed to create task" });
-  }
+        res.status(201).json({ success: true, message: "Task created successfully" });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: "Failed to create task" });
+    }
 };
 
 module.exports = { createTask };
