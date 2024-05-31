c3dd9dfce4bd99de5685bcbbefc1850d49ecc3e5
Aadil-Shabir <aadil.shabir13@gmail.com>
2024-05-31 19:31:40 +0500

    check-mongodb-connection

--- config/database.js
+++ config/database.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
 
-const connectToDatabase = () => {
-  mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
-  const db = mongoose.connection;
-  db.on("error", console.error.bind(console, "Connection error:"));
-  db.once("open", () => {
-    console.log("Database connected");
-  });
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+        });
+        console.log("Database connected");
+    } catch (error) {
+        console.error("Database connection error:", error);
+        process.exit(1); // Exit the process with failure code
+    }
 };
 
 module.exports = connectToDatabase;
