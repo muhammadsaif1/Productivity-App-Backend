3117b4a991d69016f0b9c2e777fad72ac2533be1
Saif <saif.shabir12@gmail.com>
2025-07-16 17:20:28 +0500

    fix/cors import

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const corsMiddleware = require("./middleware/corsHandler");
 connectToDatabase();
 
 const app = express();
-app.use(cors());
+app.use(corsMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(tasksRouter);
