c68ef718d77f7bfe569c0af0d55bbf811f6949e6
Saif <saif.shabir12@gmail.com>
2025-07-16 22:58:30 +0500

    fix

--- config/database.js
+++ config/database.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
-    try {
-        const connection = await mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log(`Database connected: ${connection.connection.host}`);
-    } catch (error) {
-        console.error("Database connection error:", error);
-        process.exit(1); // Exit the process with failure code
-    }
+  try {
+    const connection = await mongoose.connect(
+      process.env.MONGODB_CONNECTION_STRING,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log(`Database connected: ${connection.connection.host}`);
+  } catch (error) {
+    console.error("Database connection error:", error);
+    process.exit(1); // Exit the process with failure code
+  }
 };
 
 module.exports = connectToDatabase;
